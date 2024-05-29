3a593154406be04a00cf8f0413a0a7d28a63d44c
Kevin Colaco <kcolaco@codal.com>
2024-05-29 18:55:36 +0530

    CIT-379 #time 3h learn routing, loading and errors in next

--- NextjsProjects/test2-app/src/app/meals/page.jsx
+++ NextjsProjects/test2-app/src/app/meals/page.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { Suspense } from "react";
+import classes from "./page.module.css";
+import MealsGrid from "../../../components/meals/meals-grid";
+import Link from "next/link";
+import getMeals from "../../../lib/meals";
+
+const Meals = async () => {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={[meals]} />;
+};
 
 const MealsPage = () => {
-  return <h1>MealsPage</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself.</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense fallback={<p className={classes.loading}>Fetching meals</p>}>
+          <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
 };
 
 export default MealsPage;
